19690db455f465705a2201ec903d801db4fa392d
KALLURU BHAVESH <99259276+Bhavesh-231@users.noreply.github.com>
2022-02-27 14:37:33 +0530

    Add files via upload

---
+++ CONTACT BOOK.py
@@ -0,0 +1,17 @@
+names=[]
+phone_numbers=[]
+
+num=1
+
+for i in range(num):
+    name=[input("enter the name:")]
+    numbers=[input("enter the number :")]
+    names.append(name)
+    numbers.append(phone_numbers)
+
+#print("\t names \t\t phone_numbers")
+
+for i in names:
+    print(i)
+for i in phone_numbers:
+    print(i)
